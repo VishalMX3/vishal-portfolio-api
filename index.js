d2c82e11fca5098524f7a2b5b457ef26a80600e6
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-02 23:08:38 +0530

    updated cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 // server used to send send emails
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
 app.use(express.json());
 app.use("/", router);
 app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
