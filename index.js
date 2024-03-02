a67e9cc59cd260cd1ebc1367397c719600658cab
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-02 23:35:24 +0530

    updated cors 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000/",
+    origin: "https://vishal-portfolio-hsbr.onrender.com/",
   })
 );
 app.use(express.json());
