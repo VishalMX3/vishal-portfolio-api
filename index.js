6fbf4d6deb4a08a485e79e0ef150632cceb552fb
Vishal Pundhir <f20191055@pilani.bits-pilani.ac.in>
2024-05-27 01:17:30 +0530

    test1

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ require("dotenv").config();
 
 // server used to send send emails
 const app = express();
-// app.use(
-//   cors({
-//     origin: "https://vishal-portfolio-hsbr.onrender.com/",
-//   })
-// );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
